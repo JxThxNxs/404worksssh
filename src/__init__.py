716b164c1da2763a2332dc60b46b7cecb6c52437
JxThxNxs <151388355+JxThxNxs@users.noreply.github.com>
2025-03-29 22:07:38 +0100

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-SSHWorks Client Package
+404WorksSSH Client Package
 A feature-rich SSH client with a modern GUI built using Python and PyQt6.
 """
 
 __version__ = '1.0.0'
-__author__ = 'SSHWorks Team'
+__author__ = 'JxThxNxs'
